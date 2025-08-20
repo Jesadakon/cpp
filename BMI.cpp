999a0a1c329df2097f02f50b57ecedf05241a955
Jesadakon <68219010026@udontech.ac.th>
2025-08-20 10:36:47 +0700

    Create BMI.cpp

---
+++ BMI.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float weight,height,bmi,heightcm;
+    cout << "กรุณากรอกน้ำหนัก(kg)=";
+    cin >> weight;
+    
+    cout << "กรุณากรอกส่วนสูง(เซนติเมตร)=";
+    cin >> heightcm;
+    height = heightcm / 100; // แปลงจาก cm เป็น m
+    bmi = weight / (height*height);
+    
+    cout << "ค่าBMI ของคุณคือ = "<<bmi <<endl;
+    
+    // ตรวจสอบสถานะตามค่า BMI
+    if (bmi < 18.5) {
+        cout << "ผอม" << endl;
+        
+    }
+    else if  (bmi >= 18.5 &&bmi <23) {
+        cout << "ปกติ(สุขภาพดี)";
+        
+    }
+    else if  (bmi >= 23 &&bmi <25) {
+        cout << "น้ำหนักเกิน";
+        
+    }
+    else if  (bmi >= 25 &&bmi <30) {
+        cout << "โรคอ้วนระดับ1";
+        
+    }
+    else if  (bmi >= 30) {
+        cout << "โรคอ้วนระดับ2";
+        
+    }
+    else {
+        cout << "invalid data";
+    }
+}
